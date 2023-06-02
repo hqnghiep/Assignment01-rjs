bb4d5f37fced70670eb2d6a79e66c358902b6d13
hqnghiep <hqnfullstack@gmail.com>
2023-06-02 17:24:42 +0700

    hoàn thành navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./navbar/Navbar";
+
 const Home = () => {
-	return (
-		<div>
-			<h1>Home Page</h1>
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/home/navbar/Navbar.js
+++ src/pages/home/navbar/Navbar.js
@@ -2,6 +2,42 @@ import React from "react";
 import Card from "../../../Ui/Card";
 
 import classes from "./Navbar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faPlane,
+  faCar,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+
+const icons = { bed: faBed, plane: faPlane, car: faCar, taxi: faTaxi };
+const nabarItems = [
+  {
+    type: "Stays",
+    icon: icons.bed,
+    active: true,
+  },
+  {
+    type: "Flights",
+    icon: icons.plane,
+    active: false,
+  },
+  {
+    type: "Car rentals",
+    icon: icons.car,
+    active: false,
+  },
+  {
+    type: "Attractions",
+    icon: icons.bed,
+    active: false,
+  },
+  {
+    type: "Airport taxis",
+    icon: icons.taxi,
+    active: false,
+  },
+];
 
 const Navbar = () => {
   return (
@@ -13,6 +49,21 @@ const Navbar = () => {
             <button>Register</button>
             <button>Login</button>
           </div>
+        </div>
+        <div className={classes.layoutitem}>
+          {nabarItems.map((item) => (
+            <div
+              key={item.type}
+              className={item.active ? classes.active : undefined}
+            >
+              <FontAwesomeIcon
+                icon={item.icon}
+                color="white"
+                className={classes.type}
+              />
+              <span>{item.type}</span>
+            </div>
+          ))}
         </div>
       </Card>
     </div>
