61b4e37000fbd00f935b114ed30418ac2d5f11e6
hqnghiep <hqnfullstack@gmail.com>
2023-06-02 16:33:16 +0700

    xây dựng navbar

--- src/pages/home/navbar/Navbar.js
+++ src/pages/home/navbar/Navbar.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Card from "../../../Ui/Card";
+
+import classes from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className={classes.background}>
+      <Card>
+        <div className={classes.layout}>
+          <h3>Booking Website</h3>
+          <div>
+            <button>Register</button>
+            <button>Login</button>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default Navbar;
